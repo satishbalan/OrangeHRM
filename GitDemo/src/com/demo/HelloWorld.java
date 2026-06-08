95969d33fcb88a06ecd813b1976c06624fc87472
satis <satishsuper59@gmail.com>
2026-06-08 09:45:17 +0530

    Modified HelloWorld

--- GitDemo/src/com/demo/HelloWorld.java
+++ GitDemo/src/com/demo/HelloWorld.java
@@ -1,7 +1,7 @@
-public class HelloWorld {
+x	public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub-Version 2");
     }
 
 }
